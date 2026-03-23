55b9173efb4e8f50e56b11e08e86b02052801596
james.swanson <james.swanson@pinecrest.edu>
2026-03-23 16:57:27 -0400

    I donne know what im doing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Janks_test.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.linearOpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous(name = "Our Autonomous")
+public class Janks_test extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor motor;
+        motor = hardwareMap.get(DcMotor.class, "motor");
+
+        motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+
+        motor.setPower(1);
+        sleep(1000);
+        motor.setPower(0);
+    }
+}
