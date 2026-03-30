e371be0271faed7da56bc83c4366fd474a22d08b
Carter-Hamaway <Carter.hamaway@pinecrest.edu>
2026-03-30 16:38:06 -0400

    LEARN CODE

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CarterTeleop.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@TeleOp(name = "Carter Teleop")
+public class CarterTeleop extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor motor;
+        motor = hardwareMap.get(DcMotor.class, "motor");
+        motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor.setZeroPowerBehavior(BRAKE);
+
+        waitForStart();
+
+
+        while(opModeIsActive()){
+            if(gamepad1.square){
+                motor.setPower(1);  //Spins the Motor Forward
+            }
+            else if(gamepad1.circle){
+                motor.setPower(-1);  //Spins the Motor Backwards
+            }
+            else{
+                motor.setPower(0);  //Stops the motor
+            }
+
+            double triggerValue = gamepad1.right_trigger;
+
+            double joystickValue = gamepad1.left_stick_x;
+
+            // Scale the motor power based on the trigger value
+            motor.setPower(triggerValue);
+            motor.setPower(joystickValue);
+
+            // Display the trigger value and motor power on the telemetry
+            telemetry.addData("Stick value  ::  ", joystickValue);
+            telemetry.addData("Trigger Value  ::  ", triggerValue);
+            telemetry.addData("Motor power  ::  ", motor.getPower());
+
+            telemetry.update();
+
+
+
+
+        }
+    }
+}
